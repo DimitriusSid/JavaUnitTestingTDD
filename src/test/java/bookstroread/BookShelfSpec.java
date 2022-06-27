b90b8ac90263c681a733ef268dc27336137f7b57
DimitriusSid <dmitrij.schidr@gmail.com>
2022-06-27 16:52:20 +0300

    Using @DisplayName and parameters of Constructor and Method

--- src/test/java/bookstroread/BookShelfSpec.java
+++ src/test/java/bookstroread/BookShelfSpec.java
@@ -1,15 +1,21 @@
 package bookstroread;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@DisplayName("<= BookShelf Specification =>")
 public class BookShelfSpec {
-
+    private BookShelfSpec(TestInfo testInfo) {
+        System.out.println("Working on test " + testInfo.getDisplayName());
+    }
     @Test
-    public void shelfEmptyWhenNoBookAdded() {
+    void shelfEmptyWhenNoBookAdded(TestInfo testInfo) {
+        System.out.println("Working in test case " + testInfo.getDisplayName());
         BookShelf shelf = new BookShelf();
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), "BookShelf should be empty");
